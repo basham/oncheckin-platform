423e5ae76724a89f9718a61acada5d1b6ca86ec5
Chris Basham <chris@bash.am>
2026-01-24 13:55:28 -0500

    Add createClub action

---
+++ packages/app/src/actions/createClub.ts
@@ -0,0 +1,23 @@
+import { getContext, schemaVersion, now, Entity, Root } from "@src/core";
+
+export async function createClub({ name }) {
+	const context = await getContext();
+	const club = Root.create({
+		meta: {
+			name,
+			schemaVersion,
+		},
+		entities: {},
+	});
+	const accountClub = Entity.create({
+		meta: {
+			lastViewedAt: now(),
+			schemaVersion,
+		},
+		club: {
+			root: club,
+		},
+	});
+	context.me.root.entities.$jazz.set(accountClub.$jazz.id, accountClub);
+	return club.$jazz.id;
+}
--- packages/app/src/pages/orgs/new.js
+++ packages/app/src/pages/orgs/new.js
@@ -1,4 +1,5 @@
-import { getContext, schemaVersion, now, Entity, Root } from "@src/core";
+import { createClub } from "@src/actions";
+import { getProjection } from "@src/computed";
 
 export async function get() {
 	const h1 = "New organization";
@@ -7,26 +8,10 @@ export async function get() {
 }
 
 export async function post({ request }) {
-	const context = await getContext();
 	const formData = await request.formData();
 	const name = formData.get("name");
-	const club = Root.create({
-		meta: {
-			name,
-			schemaVersion,
-		},
-		entities: {},
-	});
-	const accountClub = Entity.create({
-		meta: {
-			lastViewedAt: now(),
-			schemaVersion,
-		},
-		club: {
-			root: club,
-		},
-	});
-	context.me.root.entities.$jazz.set(accountClub.$jazz.id, accountClub);
-	const redirect = `/orgs/?id=${club.$jazz.id}`;
+	const clubId = await createClub({ name });
+	const { org } = await getProjection(clubId);
+	const { url: redirect } = org;
 	return { redirect };
 }
